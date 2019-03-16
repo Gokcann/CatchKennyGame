f81e216c8b0196fcd85b971acf8f9d163379363c
Gokcan <gokcan.akoya@icloud.com>
2019-03-16 23:21:48 +0300

    Initial Commit

---
+++ Kenny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kenny
+//
+//  Created by Gökcan Akoya on 16.03.2019.
+//  Copyright © 2019 Gökcan Akoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
